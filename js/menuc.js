9a80cf9524a5885aa50227ed5f601de9612370d3
Daniel <109938120+RDani1001@users.noreply.github.com>
2022-08-05 02:39:56 -0500

    Add files via upload

---
+++ js/menuc.js
@@ -0,0 +1,21 @@
+(function(){
+    const titleQuestions = [...document.querySelectorAll('.menu__title')];
+    console.log(titleQuestions)
+
+    titleQuestions.forEach(question =>{
+        question.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = question.nextElementSibling;
+            let addPadding = question.parentElement.parentElement;
+
+            addPadding.classList.toggle('menu__padding--add');
+            question.children[0].classList.toggle('menu__arrow--rotate');
+
+            if(answer.clientHeight === 0){
+                height = answer.scrollHeight;
+            }
+
+            answer.style.height = `${height}px`;
+        });
+    });
+})();
